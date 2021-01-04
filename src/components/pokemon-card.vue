d0300c7ffae92d01f3d3bcb7a5d85d3a8a54583c
Adrian-DG <adrian96guilamo@gmail.com>
2021-01-04 18:43:14 -0400

    show pokemon cards ready

---
+++ src/components/pokemon-card.vue
@@ -0,0 +1,51 @@
+<template>
+  <b-card
+    :title="updatedName"
+    :img-src="pokemon.sprites.front_default"
+    img-top
+    tag="type"
+  >
+    <b-card-text>
+      <p>
+        <strong>{{ updatedName }}</strong> is a pokemon of the following type:
+      </p>
+      <span
+        class="type-tag"
+        v-for="(type, index) in pokemon.types"
+        :key="index"
+      >
+        {{ type.type.name }}
+      </span>
+    </b-card-text>
+    <b-button block variant="info">Details</b-button>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "pokemon-card",
+  props: ["pokemon"],
+  computed: {
+    updatedName() {
+      const name = Array.from(this.pokemon.name);
+      name[0] = name[0].toUpperCase();
+      return name.join("");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card-img-top {
+  border-bottom: 1px solid gainsboro;
+}
+.card-title {
+  font-size: 1.2rem;
+}
+.type-tag {
+  background-color: gainsboro;
+  border-radius: 0.3em;
+  padding: 0.2em 0.5em;
+  margin-right: 0.5em;
+}
+</style>
--- src/components/pokemon-list.vue
+++ src/components/pokemon-list.vue
@@ -1,15 +1,111 @@
 <template>
   <b-container fluid>
     <b-row>
-      <b-col cols="6">Pokemons</b-col>
-      <b-col cols="6"></b-col>
+      <b-col class="title" col md="6" lg="6">
+        <h4>Pokemons</h4>
+      </b-col>
+      <b-col class="search" col sm="12" md="6" lg="6">
+        <b-form @submit.prevent="SearchByName">
+          <b-input-group>
+            <b-form-input
+              v-model="pokemonName"
+              placeholder="pokemon name"
+            ></b-form-input>
+            <b-input-group-append>
+              <b-button type="submit" variant="secondary">search</b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </b-form>
+      </b-col>
     </b-row>
-    <ul class="pokemon-list"></ul>
+    <hr />
+
+    <ul class="pokemon-list">
+      <li class="list-item" v-for="(pokemon, index) in pokemons" :key="index">
+        <pokemon-card :pokemon="pokemon" />
+      </li>
+    </ul>
   </b-container>
 </template>
 
 <script>
+import PokemonCard from "./pokemon-card.vue";
 export default {
   name: "pokemon-list",
+  data() {
+    return {
+      pokemons: [],
+      pokemonName: "",
+    };
+  },
+  components: {
+    "pokemon-card": PokemonCard,
+  },
+  created() {
+    this.$store.dispatch("fetchApi", { endPoint: "pokemon" });
+  },
+  beforeMount() {
+    this.pokemons = this.$store.getters.getPokemons;
+  },
+  methods: {
+    SearchByName() {
+      this.$store.dispatch("fetchPokemonByName", { name: this.pokemonName });
+    },
+  },
 };
 </script>
+
+<style scoped>
+.pokemon-list {
+  max-height: 55vh;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.list-item {
+  list-style: none;
+  margin: 0 1em 1em 1em;
+}
+
+@media screen and (min-width: 1024px) {
+  .pokemon-list {
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    overflow-y: auto;
+  }
+  .list-item {
+    width: 15%;
+  }
+}
+
+@media screen and (min-width: 768px) and (max-width: 1024px) {
+  .pokemon-list {
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: flex-start;
+    overflow-y: auto;
+  }
+  .list-item {
+    width: 25%;
+  }
+}
+
+@media screen and (max-width: 425px) {
+  .pokemon-list {
+    max-height: 60vh !important;
+    flex-direction: column;
+    justify-content: center !important;
+    align-items: center !important;
+    overflow-y: hidden;
+    overflow-x: auto;
+  }
+  .list-item {
+    width: 100%;
+    margin: 0.5em 3em 0.5em -1.5em;
+  }
+  .title {
+    display: none;
+  }
+}
+</style>
