c00ace2db73127d85dd73535e746f7ff36853e7a
Adrian-DG <adrian96guilamo@gmail.com>
2021-01-06 21:04:43 -0400

    rollback

--- dist/js/chunk-2d0cc864.4189c591.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cc864"],{"4dfd":function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("b-container",[n._v("region name: "+n._s(this.$route.params.name)+" ")])},r=[],c={name:"region-details"},s=c,i=t("2877"),o=Object(i["a"])(s,a,r,!1,null,null,null);e["default"]=o.exports}}]);
-//# sourceMappingURL=chunk-2d0cc864.4189c591.js.map
